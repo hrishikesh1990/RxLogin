53d469380b816e920369053e4998dcab1c83ca7e
Saravanan Vijayakumar <saravanan.vijayakumar@photoninfotech.net>
2019-05-21 13:47:02 +0530

    Initial Commit

---
+++ RxLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxLogin
+//
+//  Created by Saravanan Vijayakumar on 21/05/19.
+//  Copyright © 2019 Saravanan Vijayakumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
